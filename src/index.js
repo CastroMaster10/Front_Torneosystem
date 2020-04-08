ec1a3960daa298281112afe1f1765b618394ad4a
CastroMaster10 <rudacate02@hotmail.com>
2020-04-08 01:27:43 -0500

    se dividieron en carpetas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Helloworld from './components/HelloWorld'
+import './assets/'
 
 ReactDOM.render(<Helloworld />, document.getElementById("app"));
 
